8b368d17d51f8de59db49bba757c872286489fe1
Anton Ekblad <anton@ekblad.cc>
2017-01-11 00:01:32 +0100

    Fix `haste-app build` with locally installed Haste.

--- bin-src/Main.hs
+++ bin-src/Main.hs
@@ -1,13 +1,20 @@
 module Main where
-import Control.Shell (shell, cmdline, ExitReason (..))
+import Control.Shell hiding (run, hPutStrLn)
 import System.Exit (exitFailure)
 import System.IO (hPutStrLn, stderr)
 
-import Options
 import Commands
+import Environment
+import Options
 
 main = do
-  result <- shell $ run availableCommands cmdline
+  result <- shell $ do
+    path <- getEnv "PATH"
+    appdir <- withAppDirectory appName pure
+    let mypath = case os of
+          Windows -> (appdir </> "bin") ++ ";" ++ path
+          _       -> (appdir </> "bin") ++ ":" ++ path
+    withEnv "PATH" mypath $ run availableCommands cmdline
   case result of
     Left (Failure msg) -> hPutStrLn stderr msg >> exitFailure
     _                  -> return ()
