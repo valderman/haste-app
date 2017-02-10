a3d26e2efd5f97f478d9f07e701a8faa55109be8
Anton Ekblad <anton@ekblad.cc>
2017-02-10 15:27:39 +0100

    Add client exception handling through ErrorT.

--- src/Haste/App/Client/Type.hs
+++ src/Haste/App/Client/Type.hs
@@ -1,10 +1,30 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 -- | The @Client@ monad type.
 module Haste.App.Client.Type where
-import Haste.Concurrent (MonadConc, CIO)
+import Haste.Concurrent (MonadConc (..), CIO, concurrent)
 import Control.Monad.IO.Class (MonadIO)
-import Haste.Events (MonadEvent)
+import Haste.Events (MonadEvent (..))
+
+import Control.Exception
+import Control.Monad.Error
+import Data.Typeable
+
+data ClientError = ClientError String
+  deriving (Typeable, Show)
+
+instance Error ClientError where
+  strMsg = ClientError
+
+instance (Error e, MonadConc m) => MonadConc (ErrorT e m) where
+  liftCIO = lift . liftCIO
+  fork m  = lift $ fork $ do
+    Right x <- runErrorT m
+    return x
+
+instance MonadEvent Client where
+  mkHandler f = return $ \x -> concurrent $ do
+    void $ runErrorT $ runClient $ f x
 
 -- | A client program running in the browser.
-newtype Client a = Client {runClient :: CIO a}
-  deriving (Functor, Applicative, Monad, MonadIO, MonadConc, MonadEvent)
+newtype Client a = Client {runClient :: ErrorT ClientError CIO a}
+  deriving (Functor, Applicative, Monad, MonadIO, MonadConc, MonadError ClientError)
