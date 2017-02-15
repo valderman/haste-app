ad435b47722995592fa5116a60a34fde52a26d99
Anton Ekblad <anton@ekblad.cc>
2017-02-15 13:37:09 +0100

    Clean up Haste.App.Client.

--- src/Haste/App/Client.hs
+++ src/Haste/App/Client.hs
@@ -1,55 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Haste.App.Client
   ( Client, MonadClient (..)
-  , runClient, sleep
+  , runClient
   ) where
-import Control.Exception
-import Control.Monad
-import Control.Monad.IO.Class
-import Data.Maybe (isJust)
-import Data.IORef
-import qualified Data.Map.Strict as Map
+import Control.Monad ((<=<))
 import Haste.Serialize
 import Haste.JSON
 import Haste.Concurrent
 import Haste.WebSockets
-import qualified Haste.JSString as JSS
-import System.IO.Unsafe
+import qualified Haste.JSString as JSS (concat)
 import Haste.App.Client.Type
 import Haste.App.Transport
 import Haste.App.Protocol
-import Haste.App.Sandbox
-
--- For default onDisconnect handler
-import Haste (toJSString, catJSStr, setTimer, Interval (..))
-import Haste.DOM.JSString
-import Haste.Events
-
--- For Client MonadEvent instance
-import Haste.Events (MonadEvent (..))
-
--- | A barrier.
-newtype Barrier = Barrier (MVar ())
-
--- | Open a barrier. Calling 'await' on the barrier will no longer block.
-openBarrier :: Barrier -> Client ()
-openBarrier (Barrier v) = liftCIO $ void $ tryPutMVar v ()
-
--- | Block until the given barrier becomes open. If it is already open, pass
---   immediately.
-await :: Barrier -> Client ()
-await (Barrier v) = liftCIO $ readMVar v
-
--- | Create a new barrier. The barrier is initially closed.
-newBarrier :: Client Barrier
-newBarrier = Barrier <$> liftCIO newEmptyMVar
-
--- | Sleep @n@ milliseconds.
-sleep :: Int -> Client ()
-sleep n = do
-  b <- newBarrier
-  setTimer (Once n) $ openBarrier b
-  await b
+import Haste (toJSString)
+import Haste.App.Sandbox (callSandbox)
 
 instance MonadClient Client where
   remoteCall ep@(LocalNode{}) pkt n = do
