1eb840f3122f133d9f476f931892a689a8300d5e
Anton Ekblad <anton@ekblad.cc>
2017-05-12 14:20:14 +0200

    Simplify Remotable class.

--- src/Haste/App/Remote.hs
+++ src/Haste/App/Remote.hs
@@ -43,7 +43,7 @@ type family HaskF c a where
 type Dispatch dom cli =
   ( Node (Affinity dom)
   , Allowed (Affinity dom) (Affinity cli)
-  , Remotable (Affinity cli) (Affinity dom) cli
+  , Remotable cli
   , HaskF (Affinity cli) dom ~ cli
   )
 
@@ -54,18 +54,17 @@ type Export dom =
   )
 
 -- | A client-side frontend for a 'Remote' function.
-class Remotable (cli :: * -> *) (m :: * -> *) a where
+class Remotable a where
   -- | Plumbing for turning a 'StaticKey' into a remote function, callable on
   --   the client.
-  dispatch' :: Proxy cli -> Proxy m -> Endpoint -> StaticKey -> [JSON] -> a
+  dispatch' :: Endpoint -> StaticKey -> [JSON] -> a
 
-instance (Serialize a, Remotable cli m b) => Remotable cli m (a -> b) where
-  dispatch' pc pm ep k xs x = dispatch' pc pm ep k (toJSON x : xs)
+instance {-# OVERLAPPING #-} (Serialize a, Remotable b) => Remotable (a -> b) where
+  dispatch' ep k xs x = dispatch' ep k (toJSON x : xs)
 
-instance forall cli m a. (MonadClient cli, Node m, Serialize a)
-         => Remotable cli m (cli a) where
-  dispatch' _ pm ep k xs = do
-    Right x <- fromJSON <$> call ep pm k (reverse xs)
+instance (MonadClient cli, Serialize a) => Remotable (cli a) where
+  dispatch' ep k xs = do
+    Right x <- fromJSON <$> call ep k (reverse xs)
     return x
 
 -- | A function that may act as a server-side callback. That is, one where all
@@ -83,18 +82,15 @@ instance (Serialize a, Remote m b) => Remote m (a -> b) where
 instance (Affinity (m a) ~ m, Mapping m a, Res (m a) ~ a) => Remote m (m a) where
   blob m _ env = invoke env m
 
-call :: forall (from :: * -> *) (to :: * -> *). MonadClient from
-     => Endpoint -> Proxy to -> StaticKey -> [JSON] -> from JSON
-call ep pm k xs = do
+call :: MonadClient from => Endpoint -> StaticKey -> [JSON] -> from JSON
+call ep k xs = do
   n <- getNonce
   remoteCall ep (encodeJSON $ toJSON $ (ServerCall n k xs)) n
 
 -- | Serializify any function of type @a -> ... -> b@ into a corresponding
 --   function of type @[JSON] -> Server JSON@, with the same semantics.
 --   This allows the function to be called remotely via a static pointer.
-remote :: forall dom. Export dom
-       => dom
-       -> Import dom
+remote :: forall dom. Export dom => dom -> Import dom
 remote f = Import $ \env xs ->
   toJSON <$> (blob f xs env :: CIO (Hask (Affinity dom) (Res dom)))
 
@@ -111,24 +107,12 @@ remote f = Import $ \env xs ->
 --
 -- > f' = static (remote f)
 -- > main = runApp $ dispatch f' x0 x1 ...
-dispatch :: forall cli dom. Dispatch dom cli
-         => StaticPtr (Import dom)
-         -> cli
-dispatch f = dispatch' (Proxy :: Proxy (Affinity cli))
-                       (Proxy :: Proxy (Affinity dom))
-                       (endpoint (Proxy :: Proxy (Affinity dom)))
-                       (staticKey f)
-                       []
+dispatch :: forall cli dom. Dispatch dom cli => StaticPtr (Import dom) -> cli
+dispatch f = dispatch' ep (staticKey f) []
+  where ep = endpoint (Proxy :: Proxy (Affinity dom))
 
 -- | Like 'dispatch', but makes a direct call to the server, and overrides
 --   the its default endpoint. The server node must be directly attached to
 --   the client making the call.
-dispatchTo :: forall cli dom. (Dispatch dom cli)
-           => Endpoint
-           -> StaticPtr (Import dom)
-           -> cli
-dispatchTo ep f = dispatch' (Proxy :: Proxy (Affinity cli))
-                            (Proxy :: Proxy (Affinity dom))
-                            ep
-                            (staticKey f)
-                            []
+dispatchTo :: Dispatch dom cli => Endpoint -> StaticPtr (Import dom) -> cli
+dispatchTo ep f = dispatch' ep (staticKey f) []
